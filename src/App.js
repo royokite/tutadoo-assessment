0746bc304cab60cf0a2588b0c4f626e73b8c3659
Roy OKite <royokite@gmail.com>
2023-05-04 16:39:31 +0300

    Configure React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Testing Style!
-    </h1>
+    <section>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Testing Style!
+      </h1>
+      <Button variant="success">Click Me</Button>
+    </section>
+    
   )
 }
