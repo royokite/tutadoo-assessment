6b4b6b6b86d72e5be28b59ac77e2bb6a5d0ba7df
Roy OKite <royokite@gmail.com>
2023-05-05 16:07:58 +0300

    :art: Add remaining item images
    Add the other product pictures and render.

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,4 +1,8 @@
 import item1 from "./images/product-pic1.png"
+import item2 from "./images/product-pic2.png"
+import item3 from "./images/product-pic3.png"
+import item4 from "./images/product-pic4.png"
+import item5 from "./images/product-pic5.png"
 
 export const ProductList = [
     {
@@ -7,7 +11,17 @@ export const ProductList = [
         price: "50.00"
     },
     {
-        image: item1, 
+        image: item2, 
+        title: "Apollo Running Short",
+        price: "50.00"
+    },
+    {
+        image: item3, 
+        title: "Apollo Running Short",
+        price: "50.00"
+    },
+    {
+        image: item4, 
         title: "Apollo Running Short",
         price: "50.00"
     },
@@ -17,17 +31,7 @@ export const ProductList = [
         price: "50.00"
     },
     {
-        image: item1, 
-        title: "Apollo Running Short",
-        price: "50.00"
-    },
-    {
-        image: item1, 
-        title: "Apollo Running Short",
-        price: "50.00"
-    },
-    {
-        image: item1, 
+        image: item3, 
         title: "Apollo Running Short",
         price: "50.00"
     },
