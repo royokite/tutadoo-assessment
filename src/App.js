712cadc47fe7bc1b6f057c2daf45dc11804e7e41
Roy OKite <royokite@gmail.com>
2023-05-05 11:43:11 +0300

    :construction: Set up file structure
    Set up project file structure.
    Change Logo and Title

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
+import Home from './screens/Home';
+import MainButton from './components/Button';
 
 export default function App() {
   return (
     <section>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Testing Style!
-      </h1>
-      <Button variant="success">Click Me</Button>
-    </section>
-    
+      <Home/>
+      <MainButton/>
+    </section>    
   )
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+import Button from 'react-bootstrap/Button';
+
+export default function MainButton() {
+    return (
+        <Button variant="success">Click Me</Button>
+    );
+}
