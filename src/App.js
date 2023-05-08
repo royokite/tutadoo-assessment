30493fe839942396ec3f06fe57f9c8847ee2210b
Roy OKite <royokite@gmail.com>
2023-05-08 10:49:46 +0300

    :sparkles: Set up navbar
    Add navbar component
    Use icons for cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './screens/Home';
 import MainButton from './components/Button';
+import Navigation from './components/Navigation';
 
 export default function App() {
   return (
     <section>
+      <Navigation/>
       <Home/>
       <MainButton/>
     </section>    
